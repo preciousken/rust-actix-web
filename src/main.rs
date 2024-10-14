a391f4f042498f7b9effad612e8485dcb913b089
preciousken <pkehinde27@gmail.com>
2024-10-14 08:17:23 +0100

    this only for folder structuring in rust;

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,25 @@
 // Shared Mutable State
 
-use actix_web::{web, App, HttpServer};
 use std::sync::Mutex;
+
+use actix_web::{web, App, HttpServer};
+
+mod routes;
+use routes::*;
+
+// async fn index(data: web::Data<AppStateWithCounter>) -> String {
+//     let mut counter = data.counter.lock().unwrap(); // <- get counter's MutexGuard
+//     *counter += 1; // <- access counter inside MutexGuard
+
+//     format!("Request number: {counter}") // <- response with count
+// }
 
 struct AppStateWithCounter {
     counter: Mutex<i32>, // <- Mutex is necessary to mutate safely across threads
 }
 
-async fn index(data: web::Data<AppStateWithCounter>) -> String {
-    let mut counter = data.counter.lock().unwrap(); // <- get counter's MutexGuard
-    *counter += 1; // <- access counter inside MutexGuard
-
-    format!("Request number: {counter}") // <- response with count
-}
-
 #[actix_web::main]
-async fn main() -> std::io::Result<()> {
+ async fn main() -> std::io::Result<()> {
     // Note: web::Data created _outside_ HttpServer::new closure
     let counter = web::Data::new(AppStateWithCounter {
         counter: Mutex::new(0),
---
+++ src/routes/index.rs
@@ -0,0 +1,13 @@
+use actix_web::{web,};
+use std::sync::Mutex;
+
+struct AppStateWithCounter {
+    counter: Mutex<i32>, // <- Mutex is necessary to mutate safely across threads
+}
+
+async fn index(data: web::Data<AppStateWithCounter>) -> String {
+    let mut counter = data.counter.lock().unwrap(); // <- get counter's MutexGuard
+    *counter += 1; // <- access counter inside MutexGuard
+
+    format!("Request number: {counter}") // <- response with count
+}
